92600485a6181781e5de8b27ff57f027990e2430
DESKTOP-VGMF4QA\nguye <nguyenkimlanh2010@gmail.com>
2021-05-04 22:52:33 +0700

    layout

--- src/containers/MainDashboard/index.js
+++ src/containers/MainDashboard/index.js
@@ -6,77 +6,104 @@ import VN30DerivativeChart from "components/charts/main_charts/VN30DerivativeCha
 import ForeignDerivativeChart from "components/charts/main_charts/ForeignDerivativeChart";
 
 import {
-  Container, Row, Col
+    Container, Row, Col, Form
 } from 'react-bootstrap';
 
 class MainDashboardScreen extends React.Component {
-  
-  constructor(props){
-    super(props);
 
-    this.chartRefs = [];
-    //this.chartRefs.push(this.chartC1Ref = React.createRef());
-    this.chartRefs.push(this.chartC2Ref = React.createRef());
-    this.chartRefs.push(this.chartC3Ref = React.createRef());
-  }
+    constructor(props) {
+        super(props);
 
-  componentDidMount() {
-    const { fetchAllData } = this.props;
-    fetchAllData();
-
-    for (var i = 0; i < this.chartRefs.length; i++) {
-      for (var j = 0; j < this.chartRefs.length; j++) {
-        if (i != j) {
-          //this.chartRefs[i].current.registerOtherCharts(this.chartRefs[j].current.chart);
-        }
-      }
+        this.chartRefs = [];
+        //this.chartRefs.push(this.chartC1Ref = React.createRef());
+        this.chartRefs.push(this.chartC2Ref = React.createRef());
+        this.chartRefs.push(this.chartC3Ref = React.createRef());
     }
-  }
 
-  componentWillUnmount() {
-  }
+    componentDidMount() {
+        const {fetchAllData} = this.props;
+        fetchAllData();
 
-  render() {
-    console.log("before render chart", this.props);
-    return (
-      <Container fluid>
-        <Row>
-            {/* <Col>
-              <ForeignDerivativeChart ref={this.chartC1Ref} data={{openPrice: stockPrice.openPrice}} />
-            </Col> */}
-            <Col>
-              <VN30DerivativeChart ref={this.chartC2Ref} data={{chartData: this.props.PSOutbound}} />
-            </Col>
-            <Col>
-              <VN30DerivativeChart ref={this.chartC3Ref} data={{chartData: this.props.PSOutbound}} />
-            </Col>
-        </Row>
-      </Container>
-    )
-  }
+        for (var i = 0; i < this.chartRefs.length; i++) {
+            for (var j = 0; j < this.chartRefs.length; j++) {
+                if (i != j) {
+                    //this.chartRefs[i].current.registerOtherCharts(this.chartRefs[j].current.chart);
+                }
+            }
+        }
+    }
+
+    componentWillUnmount() {
+    }
+
+    render() {
+        console.log("before render chart", this.props);
+        return (
+            <Container fluid>
+                <Row style={{height: '100vh'}}>
+                    <Col>
+                        <Row style={{height: '30vh'}}>
+                            <VN30DerivativeChart ref={this.chartC3Ref} data={{chartData: this.props.PSOutbound}}/>
+                        </Row>
+                        <Row style={{height: '30vh'}}>
+                            <VN30DerivativeChart ref={this.chartC3Ref} data={{chartData: this.props.PSOutbound}}/>
+                        </Row>
+                        <Row style={{height: '30vh'}}>
+                            <VN30DerivativeChart ref={this.chartC3Ref} data={{chartData: this.props.PSOutbound}}/>
+                        </Row>
+                    </Col>
+                    <Col>
+                        <Row style={{height: '25vh'}}>
+                            <VN30DerivativeChart ref={this.chartC3Ref} data={{chartData: this.props.PSOutbound}}/>
+                        </Row>
+                        <Row style={{height: '25vh'}}>
+                            <VN30DerivativeChart ref={this.chartC3Ref} data={{chartData: this.props.PSOutbound}}/>
+                        </Row>
+                        <Row style={{height: '10vh'}}></Row>
+                        <Row style={{height: '30vh'}}>
+                            <Col></Col>
+                            <Col> <Form.Control type="date"></Form.Control></Col>
+                            <Col></Col>
+                        </Row>
+                    </Col>
+                    <Col>
+                        <Row style={{height: '30vh'}}>
+                            <VN30DerivativeChart ref={this.chartC3Ref} data={{chartData: this.props.PSOutbound}}/>
+                        </Row>
+                        <Row style={{height: '30vh'}}>
+                            <VN30DerivativeChart ref={this.chartC3Ref} data={{chartData: this.props.PSOutbound}}/>
+                        </Row>
+                        <Row style={{height: '30vh'}}>
+                            <VN30DerivativeChart ref={this.chartC3Ref} data={{chartData: this.props.PSOutbound}}/>
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
+        )
+    }
 }
 
 const mapDispatchToProps = (dispatch) => {
-  const {actions} = require("../../redux/StockPriceRedux");
-  return {
-    fetchAllData: () =>{
-       actions.fetchPSOutboundData(dispatch);
-       actions.fetchBusdOutboundData(dispatch);
-       actions.fetchBusdNNOutboundData(dispatch);
-       actions.fetchBuySellNNOutboundData(dispatch);
-       actions.fetchSuuF1OutboundData(dispatch);
-       actions.fetchArbitUnwindData(dispatch);
+    const {actions} = require("../../redux/StockPriceRedux");
+    return {
+        fetchAllData: () => {
+            actions.fetchPSOutboundData(dispatch);
+            actions.fetchBusdOutboundData(dispatch);
+            actions.fetchBusdNNOutboundData(dispatch);
+            actions.fetchBuySellNNOutboundData(dispatch);
+            actions.fetchSuuF1OutboundData(dispatch);
+            actions.fetchArbitUnwindData(dispatch);
+        }
     }
-  }
 };
 
 const mapStateToProps = (state) => {
-  return {
-    PSOutbound: state.stockPrice.PSOutbound
-  }
+    return {
+        PSOutbound: state.stockPrice.PSOutbound
+    }
 }
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(MainDashboardScreen);
