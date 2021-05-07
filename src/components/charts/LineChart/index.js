3c8eb5431918e064da9445d3487e9b65081fef6a
hoang <hoangdh91@gmail.com>
2021-05-08 00:08:21 +0700

    More options

--- src/components/charts/LineChart/index.js
+++ src/components/charts/LineChart/index.js
@@ -8,17 +8,44 @@ export default class LineChart extends BaseChart {
   }
 
   buildChart() {
+    var chart = this.chart;
+    this.chart.addEventListener("wheel", (e) => {
+      e.preventDefault();
+      
+      if(e.clientX < chart.plotArea.x1 || e.clientX > chart.plotArea.x2 || e.clientY < chart.plotArea.y1 || e.clientY > chart.plotArea.y2)
+        return;
+        
+      var axisX = chart.axisX[0];
+      var viewportMin = axisX.get("viewportMinimum"),
+          viewportMax = axisX.get("viewportMaximum"),
+          interval = axisX.get("minimum");
+    
+      var newViewportMin, newViewportMax;
+    
+      if (e.deltaY < 0) {
+        newViewportMin = viewportMin + interval;
+        newViewportMax = viewportMax - interval;
+      }
+      else if (e.deltaY > 0) {
+        newViewportMin = viewportMin - interval;
+        newViewportMax = viewportMax + interval;
+      }
+    
+      if(newViewportMin >= chart.axisX[0].get("minimum") && newViewportMax <= chart.axisX[0].get("maximum") && (newViewportMax - newViewportMin) > (2 * interval)){
+        chart.axisX[0].set("viewportMinimum", newViewportMin, false);
+        chart.axisX[0].set("viewportMaximum", newViewportMax);
+      }
+    });
   }
 
   getChartName() {
-    return "Chart Name";
+    return "";
   }
 
   getChartOptions() {
     const options = {
-      theme: "light2",
+      theme: "light1",
       animationEnabled: true,
-      zoomEnabled: true,
       title: {
         text: this.getChartName()
       },
@@ -26,21 +53,31 @@ export default class LineChart extends BaseChart {
         crosshair: {
           enabled: true,
           snapToDataPoint: true
-        }
+        },
+        valueFormatString: "HH:mm:ss",
+        interval: 1,
+        intervalType: "hour",
+        includeZero: false,
+
+        tickLength: 2,
+        tickColor: "red"
       },
       axisY: {
         title: "Price",
         crosshair: {
           enabled: true
-        }
+        },
+        includeZero: false
       },
       data: [{
         type: "line",
-        xValueFormatString: "HH:mm:ss",
+        xValueType: "dateTime",
         yValueFormatString: "#,##0.00",
-        dataPoints: this.dataPoints,
-        interval: 5
-      }]
+        dataPoints: this.dataPoints
+      }],
+      navigator: {
+        backgroundColor: "#D3D3D3"
+      }
     }
     return options;
   }
@@ -49,7 +86,7 @@ export default class LineChart extends BaseChart {
     let chartData = this.props.data.chartData;
     if (!chartData) chartData = [];
 
-    chartData.map(item => this.dataPoints.push({ x: item.time, y: item.price }));
+    this.chart.options.data[0].dataPoints = chartData.map(item => ({ x: item.time, y: item.price }));
     this.chart.render();
   }
 }
